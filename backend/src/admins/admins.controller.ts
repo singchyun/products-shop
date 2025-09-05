e3f4db52a087a9297a6f613cad874af25754a59a
Lee Sing Chyun <singchyun@gmail.com>
2025-09-05 22:20:25 +0800

    feat: adds admins controller.

--- backend/src/admins/admins.controller.ts
+++ backend/src/admins/admins.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { AdminsService } from './admins.service';
+import { Admin } from './admin.entity';
 
 @Controller('admins')
-export class AdminsController {}
+export class AdminsController {
+  constructor(private readonly adminsService: AdminsService) {}
+
+  @Post()
+  async create(@Body() data: Omit<Admin, 'id'>): Promise<Admin> {
+    return this.adminsService.create(data);
+  }
+
+  @Get()
+  async findAll(): Promise<Admin[]> {
+    return this.adminsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Admin> {
+    return this.adminsService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: Partial<Omit<Admin, 'id'>>,
+  ): Promise<Admin> {
+    return this.adminsService.update(id, data);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.adminsService.remove(id);
+  }
+}
