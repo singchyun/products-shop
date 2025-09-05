2c4755eed16d7e5b8b7f5b1dc3e9741f028212cd
Lee Sing Chyun <singchyun@gmail.com>
2025-09-05 21:04:20 +0800

    feat: add nestjs module "products".

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
